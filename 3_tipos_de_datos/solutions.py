6d1cca3dc0b07dad2594a74d08cfebb476b1fce6
Kenji Fushida <t19619kf@sfc.keio.ac.jp>
2024-08-19 21:09:24 +0900

    Add: solutions to basic data types

---
+++ 3_tipos_de_datos/solutions.py
@@ -0,0 +1,34 @@
+# Soluciones a los ejercicios de la seccion de Estructuras Basicas
+
+# Strings
+
+# 1
+name = "pendejo"
+print(f"Hola {name}, no te olvides de hacer la tarea")
+
+# 2
+name = "Benito"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# 3
+name = "Lionel Messi"
+quote = "Andá pa'allá, bobo"
+print(f"{quote} - {name}")
+
+# 4
+fullName = "\tRosa Melano\t"
+print(fullName.strip())
+
+# Numeros
+
+# 1
+print(4+4)
+print(10-2)
+print(4*2)
+print(int(16/2))
+
+# 2
+favoriteNumber = 5
+print(f"Mi numero favorito es {favoriteNumber}")
