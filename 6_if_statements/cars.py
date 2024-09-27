ae93e5e778b707e5bd87cc84658cb1b10c3c0447
Kenji Fushida <t19619kf@sfc.keio.ac.jp>
2024-09-27 16:46:37 +0900

    Add: If statement exercises

---
+++ 6_if_statements/cars.py
@@ -0,0 +1,21 @@
+# Declaraciones If: una declaracion que usamos para ejecutar codigo solo cuando alguna condicion sea satisfecha
+
+# Similar a la manera en que tomamos decisiones.
+# Ej: Si hoy esta lloviendo -> llevo sombrilla
+#     de lo contrario -> no llevo sombrilla
+
+# Operadores para hacer Comparaciones:
+
+# Igualdad: age == 18
+# Desigualdad: age != 18
+# Menor: age < 18
+# Igual o menor: age <= 18
+# Mayor: age > 18
+# Igual o mayor: age >= 18
+
+car = 'bmw'
+
+if car == 'bmw' or car == 'audi':
+    print('Esta marca es bmw o audi')
+else:
+    print('Esta marca de carros no es igual a bmw o audi')
---
+++ 6_if_statements/pizzas.py
@@ -0,0 +1,14 @@
+# Declaraciones If: una declaracion que usamos para ejecutar codigo solo cuando alguna condicion sea satisfecha
+
+# Similar a la manera en que tomamos decisiones.
+# Ej: Si hoy esta lloviendo -> llevo sombrilla
+#     de lo contrario -> no llevo sombrilla
+
+favoriteNumber = 10
+
+if favoriteNumber > 30:
+    print("Este numero es mayor a treinta")
+elif favoriteNumber > 20:
+    print("Este numero es mayor a 20")
+else:
+    print("Este numero es menor o igual a 20")
